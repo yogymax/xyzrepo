21bd2ec22ef0ebe3e981f2e937928da9c36287c7
yogymax <yogymax@gmail.com>
2021-04-01 12:32:43 +0530

    A changes commited

--- src/com/java/demo/one/A.java
+++ src/com/java/demo/one/A.java
@@ -4,5 +4,6 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("These changes added by 1st developer..!");
 		System.out.println("Added code in A file--by dev1");
+		System.out.println("A file Changed...");
 	}
 }
