2b8865698e39204a7d7861f97c91260f92d89bf4
yogymax <yogymax@gmail.com>
2021-04-01 12:24:52 +0530

    another developer final change

--- src/com/java/demo/one/C.java
+++ src/com/java/demo/one/C.java
@@ -2,6 +2,6 @@ package com.java.demo.one;
 
 public class C {
 	public static void main(String[] args) {
-		System.out.println("Second Developer--Added line 1");
+		System.out.println("Second Developer--Added line  1");
 	}
 }
