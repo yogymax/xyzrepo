d9f7eef452c0415e525eaa76a1929d5330b605b2
yogymax <yogymax@gmail.com>
2021-04-01 12:40:09 +0530

    Merged--Manually

--- src/com/java/demo/one/C.java
+++ src/com/java/demo/one/C.java
@@ -4,5 +4,6 @@ public class C {
 	public static void main(String[] args) {
 		System.out.println("Second Developer--Added line  1");
 		System.out.println("C file Changed By--A");
+		System.out.println("C file Changed By--B");
 	}
 }
