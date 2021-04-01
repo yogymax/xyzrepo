0310a64926d2dac81fd8d87ee0b189b00124f0f9
yogymax <yogymax@gmail.com>
2021-04-01 12:29:08 +0530

    Dev 1 Added the change into master

--- src/com/java/demo/one/A.java
+++ src/com/java/demo/one/A.java
@@ -3,5 +3,6 @@ package com.java.demo.one;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("These changes added by 1st developer..!");
+		System.out.println("Added code in A file--by dev1");
 	}
 }
