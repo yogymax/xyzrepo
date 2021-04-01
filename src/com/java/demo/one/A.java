f18e5d307a156d683c15cc4b39dfaef6260f9c70
yogymax <yogymax@gmail.com>
2021-04-01 12:18:41 +0530

    These changes commited by one

--- src/com/java/demo/one/A.java
+++ src/com/java/demo/one/A.java
@@ -1,5 +1,7 @@
 package com.java.demo.one;
 
 public class A {
-
+	public static void main(String[] args) {
+		System.out.println("These changes added by 1st developer..!");
+	}
 }
---
+++ src/com/java/demo/one/C.java
@@ -0,0 +1,5 @@
+package com.java.demo.one;
+
+public class C {
+
+}
